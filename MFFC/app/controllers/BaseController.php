5b7264f47aa8adfab2d221988554721afc7b0917
msyms <934247531@qq.com>
2019-03-20 11:30:29 +0800

    basecontroller

--- MFFC/app/controllers/BaseController.php
+++ MFFC/app/controllers/BaseController.php
@@ -1,11 +1,56 @@
 <?php
+namespace App\Controllers;
 /**
 * BaseController
 */
-class BaseController
+class BaseController extends \Pimple\Container
 {
-  public function __construct()
-  {
-  	
-  }
+　　 //服务提供中，所有提供者都在这里填写，demo中只涉及到了UserService，当然还有Route啥的。
+    protected $providers = [
+        UserServiceProvider::class
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+　　　　 //构造函数中调用注册提供者方法。
+        $this->registerProviders();
+    }
+
+    public function addProvider($provider){
+        array_push($this->providers,$provider);
+
+        return $this;
+    }
+
+    public function setProviders($providers){
+        $this->providers = [];
+
+        foreach ($providers as  $provider){
+            $this->addProvider($provider);
+        }
+    }
+
+    public function getProviders(){
+        return $this->providers;
+    }
+
+    public function __get($id)
+    {
+        return $this->offsetGet($id);
+    }
+
+    public function __set($id, $value)
+    {
+        $this->offsetSet($id,$value);
+    }
+
+　　//循环迭代之前的服务提供者，在Container的register方法中注册提供者实例，注意，这里只是提供者实例，而不是User。
+    private function registerProviders(){
+
+        foreach ($this->providers as $provider){
+            $this->register(new $provider());
+        }
+    }
 }
